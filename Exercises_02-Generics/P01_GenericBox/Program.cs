a7380af3d2d6f58bad7f89a42ad312acb48be63e
karayotov <32219166+karayotov@users.noreply.github.com>
2018-03-25 15:19:34 +0300

    secure commit

--- Exercises_02-Generics/P01_GenericBox/Program.cs
+++ Exercises_02-Generics/P01_GenericBox/Program.cs
@@ -5,14 +5,5 @@ public class Program
     static void Main(string[] args)
     {
         Box<int> intBox = new Box<int>();
-
-        Box<string> stringBox = new Box<string>();
-
-        intBox.Add(123123);
-        stringBox.Add("life in a box");
-
-
-
-        Console.WriteLine();
     }
 }
