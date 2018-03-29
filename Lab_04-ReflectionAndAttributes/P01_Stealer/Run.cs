ec577b2a5481b4742aff07a4d8b1ba3dbac4a617
karayotov <32219166+karayotov@users.noreply.github.com>
2018-03-30 00:41:20 +0300

    secure commit

---
+++ Lab_04-ReflectionAndAttributes/P01_Stealer/Run.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Run
+{
+    static void Main()
+    {
+        Spy spy = new Spy();
+
+        string result = spy.StealFieldInfo("Hacker", "username", "password"); // P01
+
+        result = spy.AnalyzeAcessModifiers("Hacker");                         // P02
+
+        Console.WriteLine(result);
+    }
+}
