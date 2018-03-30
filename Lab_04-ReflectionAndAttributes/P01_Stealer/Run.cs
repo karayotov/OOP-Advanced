4aa5a73246836426e0796a20eb33ca9c7c43a823
karayotov <32219166+karayotov@users.noreply.github.com>
2018-03-30 17:21:56 +0300

    finishing lab
     Reflection and Attributes P05, P06

--- Lab_04-ReflectionAndAttributes/P01_Stealer/Run.cs
+++ Lab_04-ReflectionAndAttributes/P01_Stealer/Run.cs
@@ -14,6 +14,7 @@ class Run
 
         result = spy.CollectGettersAndSetters("Hacker");                      // P04
 
+
         Console.WriteLine(result);
     }
 }
---
+++ Lab_04-ReflectionAndAttributes/Part_02-Attributes/Tracker.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Linq;
+using System.Reflection;
+
+public class Tracker
+{
+    public void PrintMethodsByAuthor()
+    {
+        Type type = typeof(StartUp);
+        MethodInfo[] methods = type.GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.Static);
+
+        foreach (MethodInfo methodInfo in methods)
+        {
+            if (methodInfo.CustomAttributes.Any(n => n.AttributeType == typeof(SoftUniAttribute)))
+            {
+                object[] attrs = methodInfo.GetCustomAttributes(false);
+
+                foreach (SoftUniAttribute attr in attrs)
+                {
+                    System.Console.WriteLine($"{methodInfo.Name} is written by {attr.Name}");
+                }
+            }
+        }
+    }
+}
